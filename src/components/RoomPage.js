10a09691a97f973a1a7f3596340ba0f6dc33ba87
Esau Sanchez <tonalfan@gmail.com>
2021-07-25 00:36:11 -0500

    Progesion de preguntas

--- src/components/RoomPage.js
+++ src/components/RoomPage.js
@@ -1,5 +1,6 @@
 import {useContext, useEffect, useState} from "react";
 import {SocketContext} from "../context/SocketContext";
+import {act} from "@testing-library/react";
 
 export const RoomPage = ({location}) => {
 
@@ -7,14 +8,31 @@ export const RoomPage = ({location}) => {
     const [preguntas, setPreguntas] = useState([]);
     const {socket} = useContext(SocketContext);
     const [play, setPlay] = useState(false);
-    const [actual, setActual] = useState(null);
+    const [actual, setActual] = useState(0);
 
     const handleIniciar = (e) => {
         e.preventDefault();
         socket.emit('iniciar', play);
         console.log(actual)
-        console.log(preguntas.length)
         //setActual(preguntas.find(pregunta => pregunta.id === 1))
+    }
+
+    const handleCorrecta = (e) => {
+        e.preventDefault();
+        socket.emit('respuesta-correcta', location.state);
+        actual+1 !== preguntas.length
+            ?
+        setActual(actual+1)
+            :
+            socket.emit('finish', true);
+    }
+    const handleIncorrecta = (e) => {
+        e.preventDefault();
+        actual+1 !== preguntas.length
+            ?
+            setActual(actual+1)
+            :
+            socket.emit('finish', true);
     }
 
 
@@ -29,10 +47,11 @@ export const RoomPage = ({location}) => {
         socket.on('show', (iniciar) => {
             setPlay(true);
         })
+        /*
         socket.on('current-question', (question) => {
             setActual(question)
         })
-
+        */
         return () => {
             socket.off('current-users');
             socket.off('current-preguntas')
@@ -42,7 +61,7 @@ export const RoomPage = ({location}) => {
 
 
     return(
-        <>
+        <div className="row">
             <div className="col-2">
                 <table className="table table-stripped">
                     <thead>
@@ -86,49 +105,58 @@ export const RoomPage = ({location}) => {
                     </div>
                 </div>
             </div>
-            <div className="container">
-                <div className="modal-dialog text-center">
-                    {
-                        play
-                            ?
-                                    preguntas.length === 0
-                                        ? <h1>Cargando ... </h1>
-                                        :
-                                    <div className="card" >
-                                    <div className="card-body">
-                                        <h3>{preguntas[1].descripcion}</h3>
-                                    </div>
-                                    <ul className="list-group list-group-flush ">
-                                        <li className="list-group-item d-grid gap-2">
-                                            <button
+            <div className="col-8 mt-5">
+                <div className="container">
+                    <div className="modal-dialog text-center">
+                        {
+                            play
+                                ?
+                                preguntas.length === 0
+                                    ? <h1>Cargando ... </h1>
+                                    :
+                                    <div className="modal-content card" >
+                                        <div className="card-body">
+                                            <h3>{preguntas[actual].descripcion}</h3>
+                                        </div>
+                                        <ul className="list-group list-group-flush ">
+                                            <li className="list-group-item d-grid gap-2">
+                                                <button
+                                                    className="btn btn-primary"
+                                                    onClick={handleIncorrecta}
+                                                    value={preguntas[actual].resIncorrecta1}
+                                                >
+                                                    {preguntas[actual].resIncorrecta1}
+                                                </button>
+                                            </li>
+                                            <li className="list-group-item d-grid gap-2"><button
+                                                className="btn btn-primary"
+                                                onClick={handleIncorrecta}
+                                            >
+                                                {preguntas[actual].resIncorrecta2}
+                                            </button>
+                                            </li>
+                                            <li className="list-group-item d-grid gap-2"><button
+                                                onClick={handleIncorrecta}
                                                 className="btn btn-primary"
                                             >
-                                                {preguntas[1].resIncorrecta1}
-                                            </button>
-                                        </li>
-                                        <li className="list-group-item d-grid gap-2"><button
-                                            className="btn btn-primary"
-                                        >
-                                            {preguntas[1].resIncorrecta2}
-                                        </button>
-                                        </li>
-                                        <li className="list-group-item d-grid gap-2"><button
-                                            className="btn btn-primary"
-                                        >
-                                            {preguntas[1].resIncorrecta3}
-                                        </button></li>
-                                        <li className="list-group-item d-grid gap-2"><button
-                                            className="btn btn-primary"
-                                        >
-                                            {preguntas[1].resCorrecta}
-                                        </button></li>
-                                    </ul>
-                                </div>
+                                                {preguntas[actual].resIncorrecta3}
+                                            </button></li>
+                                            <li className="list-group-item d-grid gap-2"><button
+                                                onClick={handleCorrecta}
+                                                className="btn btn-primary"
+                                            >
+                                                {preguntas[actual].resCorrecta}
+                                            </button></li>
+                                        </ul>
+                                    </div>
 
-                            :   <h1>Presione iniciar para jugar</h1>
-                    }
+                                :   <h1>Presione iniciar para jugar</h1>
+                        }
+                    </div>
                 </div>
             </div>
-        </>
+
+
+        </div>
     );
 }
